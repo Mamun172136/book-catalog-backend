51bb0757418a844ba98a6661d78b98020d8c45b8
Mamun172136 <ma1536204@gmail.com>
2023-09-03 04:49:31 +0600

    user signup validation  added

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,30 +1,30 @@
-// import { Role } from '@prisma/client';
-// import { z } from 'zod';
+import { Role } from '@prisma/client';
+import { z } from 'zod';
 
-// const create = z.object({
-//   body: z.object({
-//     name: z.string({
-//       required_error: 'Name is required',
-//     }),
-//     email: z.string({
-//       required_error: 'Email is required',
-//     }),
-//     password: z.string({
-//       required_error: 'Password is required',
-//     }),
-//     status: z.enum([...Object.values(Role)] as [string, ...string[]], {}),
-//     contactNo: z.string({
-//       required_error: 'contact is required',
-//     }),
-//     address: z.string({
-//       required_error: 'Adress is required',
-//     }),
-//     profileImg: z.string({
-//       required_error: 'profile img is required',
-//     }),
-//   }),
-// });
+const create = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'Name is required',
+    }),
+    email: z.string({
+      required_error: 'Email is required',
+    }),
+    password: z.string({
+      required_error: 'Password is required',
+    }),
+    status: z.enum([...Object.values(Role)] as [string, ...string[]], {}),
+    contactNo: z.string({
+      required_error: 'contact is required',
+    }),
+    address: z.string({
+      required_error: 'Adress is required',
+    }),
+    profileImg: z.string({
+      required_error: 'profile img is required',
+    }),
+  }),
+});
 
-// export const authValidation = {
-//   create,
-// };
+export const authValidation = {
+  create,
+};
