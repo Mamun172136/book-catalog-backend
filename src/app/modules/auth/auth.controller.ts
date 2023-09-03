8c747453ed95ffc0927731eb0ce0dc8bd05e0c79
Mamun172136 <ma1536204@gmail.com>
2023-09-03 06:27:58 +0600

    user login controller added

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -14,26 +14,26 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-// const loginUser = catchAsync(async (req: Request, res: Response) => {
-//   const { ...loginData } = req.body;
-//   const result = await authService.loginUser(loginData);
-//   // const { refreshToken, ...others } = result;
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+  const { ...loginData } = req.body;
+  const result = await authService.loginUser(loginData);
+  // const { refreshToken, ...others } = result;
 
-//   // // set refresh token into cookie
-//   // const cookieOptions = {
-//   //   secure: config.env === 'production',
-//   //   httpOnly: true,
-//   // };
+  // // set refresh token into cookie
+  // const cookieOptions = {
+  //   secure: config.env === 'production',
+  //   httpOnly: true,
+  // };
 
-//   // res.cookie('refreshToken', refreshToken, cookieOptions);
+  // res.cookie('refreshToken', refreshToken, cookieOptions);
 
-//   sendResponse<ILoginUserResponse>(res, {
-//     statusCode: 200,
-//     success: true,
-//     message: 'User signin successfully!"!',
-//     data: result,
-//   });
-// });
+  sendResponse<ILoginUserResponse>(res, {
+    statusCode: 200,
+    success: true,
+    message: 'User signin successfully!"!',
+    data: result,
+  });
+});
 
 export const authController = {
   insertIntoDB,
